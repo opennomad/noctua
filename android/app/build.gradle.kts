c0c6d3d2a73269787653c30a7af73feff2cdbec8
Matthias Johnson <matthias@opennomad.com>
2026-07-01 22:51:34 +0000

    Update all non-major dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 }
 
 flutter {
